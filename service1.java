acc2edff290a32926af223f50ecde0a0a3467480
raghuvaran <raghuvarankota@gmail.com>
2024-08-11 15:38:08 +0530

    first commit

---
+++ service1.java
@@ -0,0 +1,12 @@
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        try {
+            int[] numbers = new int[5];
+            numbers[10] = 100;  // This will cause an ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        } finally {
+            System.out.println("This block always executes.");
+        }
+    }
+}
